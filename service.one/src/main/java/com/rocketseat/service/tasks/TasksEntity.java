94c6967431bbee33860a482f6df380533d971745
MiguelMontenegroo <miguellaticiniosmontenegro@gmail.com>
2025-02-15 03:26:42 -0300

    understanding microservices

---
+++ service.one/src/main/java/com/rocketseat/service/tasks/TasksEntity.java
@@ -0,0 +1,29 @@
+package com.rocketseat.service.tasks;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TasksEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String email;
+
+    private LocalDateTime dueDate;
+
+    private boolean notified;
+}
